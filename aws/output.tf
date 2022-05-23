601b233a5b6a77d37df7982d3a1bc13f5490fd68
Ubuntu <ubuntu@ip-172-31-32-144.eu-central-1.compute.internal>
2022-05-23 12:44:25 +0000

    output

---
+++ aws/output.tf
@@ -0,0 +1,11 @@
+ output "ssh_server_public_ip_address" {
+  value = aws_instance.my_2ha.private_ip
+}
+
+ output "ssh_server_instance_private_ip_address" {
+  value = aws_instance.my_2ha.public_ip
+}
+
+output "ssh_server_security_group" {
+  value = aws_security_group.my_ubuntu.id
+}
